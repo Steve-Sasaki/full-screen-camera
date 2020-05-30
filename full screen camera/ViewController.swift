f4744548481f40da13e3cdc8b3955824e9c86645
Steve-Sasaki <wmaaaa.sa@gmail.com>
2020-05-30 13:29:44 +0900

    Initial Commit

---
+++ full screen camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  full screen camera
+//
+//  Created by 佐々木 雅斗 on 2020/05/30.
+//  Copyright © 2020 Masato SASAKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
